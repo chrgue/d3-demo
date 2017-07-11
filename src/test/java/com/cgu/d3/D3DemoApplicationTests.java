7b074cbfd349063b9cebb8b4cedc88830a4c9493
Christian G. <chr.gue@gmail.com>
2017-07-11 09:26:03 +0200

    add basic integration test

--- src/test/java/com/cgu/d3/D3DemoApplicationTests.java
+++ src/test/java/com/cgu/d3/D3DemoApplicationTests.java
@@ -2,15 +2,37 @@ package com.cgu.d3;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class D3DemoApplicationTests {
+import com.jayway.restassured.RestAssured;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class D3DemoApplicationTests {
+	
+	@LocalServerPort
+    int port;
+	
 	@Test
-	public void contextLoads() {
+	public void response200OnRoot() {
+		RestAssured.given()
+			.port(port)
+			.basePath("/")
+			.get("")
+			.then()
+			.statusCode(200);
+	}
+	
+	@Test
+	public void response200ForSingleComponent() {
+		RestAssured.given()
+			.port(port)
+			.basePath("/")
+			.get("component1")
+			.then()
+			.statusCode(200);
 	}
 
 }
